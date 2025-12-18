0aa995a1a75bd597fd8344284a5f9179b2476242
Joao Vitor <jvrezendemoura@gmail.com>
2025-12-18 06:53:57 -0300

    Creating fastapi backend

--- backend/src/main/java/com/helena_app/backend/repositories/HabitRepository.java
+++
@@ -1,9 +0,0 @@
-package com.helena_app.backend.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.helena_app.backend.models.Habit;
-
-public interface HabitRepository extends JpaRepository<Habit, Long>{
-    
-}
